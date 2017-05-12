757079d023c5018c069010a5612528114cfd8145
SkysMoon <569568313@qq.com>
2017-05-12 15:19:46 +0800

    修改目录

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
+var webServer = require("gulp-webserver");
 var gulpLoadPlugins = require('gulp-load-plugins');
 var connect = require('gulp-connect');
 var plugins = gulpLoadPlugins();
+
 gulp.task('webServer', function () {
 	plugins.connect.server();
+	gulp.src("../")
+		.pipe(webServer({
+			open: "http://localhost:8080/index.html",
+			port: 80
+		}));
 });
 
-gulp.task('default', ['webServer']);
+gulp.task('default', ['webServer'], function() {
+	console.log("gulp is doing");
+});
 
 
 
